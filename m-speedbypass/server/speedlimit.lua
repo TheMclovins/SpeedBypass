09326e402a59587da8e20624470bef8b521ad119
Mclovins Studios <cloud.llc.tcp@gmail.com>
2025-05-11 23:57:39 -0400

    Delete m-speedbypass directory

--- m-speedbypass/server/speedlimit.lua
+++
@@ -1,6 +0,0 @@
-RegisterServerEvent('speedLimiter:checkPermission')
-AddEventHandler('speedLimiter:checkPermission', function()
-    local source = source
-    local hasBypass = IsPlayerAceAllowed(source, 'mspeed.bypass')
-    TriggerClientEvent('speedLimiter:setBypass', source, hasBypass)
-end)
